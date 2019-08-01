c1ec4fc15213d58c8ead10a9a25778f74d7c2de4
tinymark <bladem@163.com>
2019-08-01 16:42:45 +0800

    add docs

---
+++ docs/precache-manifest.630b490b702f72d3b4bb340c2e9ee7c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b80580cb302a3a0b3f5d63c7f5e3484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cafde06ef3304c26b12",
+    "url": "/static/css/main.4d3a117d.chunk.css"
+  },
+  {
+    "revision": "28d8e7ceccfbdfd13a1a",
+    "url": "/static/js/2.741f2a49.chunk.js"
+  },
+  {
+    "revision": "4cafde06ef3304c26b12",
+    "url": "/static/js/main.834b2d6d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
